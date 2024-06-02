289f7e8ed1cbd92cce9ef2b7ebef1ae9a529deed
umutcagrialtinsoy <umutcaltinsoy@gmail.com>
2024-06-02 12:13:54 +0300

    entity object created.

---
+++ src/main/java/com/altinsoy/batch/entity/User.java
@@ -0,0 +1,21 @@
+package com.altinsoy.batch.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+}
